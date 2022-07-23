4a6f03c5a0d345ba1a586d794afc56f42d246a46
earl-stephens <stephensearl@hotmail.com>
2022-07-23 16:38:23 -0600

    Setup the code

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,18 @@
 package application;
 
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
+		Person p1 = new Person();
+		
+		try {
+		p1.setName(null);
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		//without try/catch, the program hits the exception and exits
+		//with try/catch, the program catches the exception and keeps going
+		System.out.println("Program completed");
 	}
 }
