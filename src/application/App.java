4b5a8302c266476caed80c32524b81543fe86f32
earl-stephens <stephensearl@hotmail.com>
2022-07-23 16:47:40 -0600

    Add finally to try catch block

--- src/application/App.java
+++ src/application/App.java
@@ -6,13 +6,27 @@ public class App {
 		Person p1 = new Person();
 		
 		try {
-		p1.setName(null);
+		p1.setName("Mary");
+		System.out.println("Still in try block");
+		//Won't print due to catching the exception
+		//immediately jumps into the catch block
 		}
 		catch (Exception e) {
 			System.out.println(e.getMessage());
+			throw e;
+			//will throw the exception again and won't print
+			//the statement below, but can use finally to 
+			//execute some kind of code after the exception
 		}
+		finally {
+			System.out.println("In finally block");
+		}
+		
 		//without try/catch, the program hits the exception and exits
 		//with try/catch, the program catches the exception and keeps going
 		System.out.println("Program completed");
 	}
 }
+/*finally lets us add some code to the end of the try catch block
+ * for allowing some code to be executed after the exception is thrown
+ * Will still execute even if the exception is not thrown */
